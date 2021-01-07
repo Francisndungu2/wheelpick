678cd9d39d41f4f40423b7a10dfb5c38525d025e
Francisndungu2 <francianmbugua@gmail.com>
2021-01-08 02:57:25 +0300

    addSetonclicklistenerANDIntent

--- app/src/main/java/com/example/wheelpick/about.java
+++ app/src/main/java/com/example/wheelpick/about.java
@@ -2,7 +2,10 @@ package com.example.wheelpick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class about extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class about extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        Button backs = (Button) findViewById(R.id.backs);
+        about.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(about.this, form.class);
+                startActivity(intent);
+
+            }
+
+        });
     }
-}
+
+    static void setOnClickListener(View.OnClickListener onClickListener) {
+
+    }
+};
