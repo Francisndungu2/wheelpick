db54710e63724b8e3d581b6146820a06c096f850
Francisndungu2 <francianmbugua@gmail.com>
2021-01-08 02:27:38 +0300

    addFindviewbyid

--- app/src/main/java/com/example/wheelpick/MainActivity.java
+++ app/src/main/java/com/example/wheelpick/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.wheelpick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button FindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
+
+
+        });
     }
-}
