771555934e6f469da7f96ed8cbe4dcd98be3daf8
Francisndungu2 <francianmbugua@gmail.com>
2021-01-08 02:30:58 +0300

    addIntentMain

--- app/src/main/java/com/example/wheelpick/MainActivity.java
+++ app/src/main/java/com/example/wheelpick/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.wheelpick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         FindRestaurantsButton = (Button)findViewById(R.id.findRestaurantsButton);
         FindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, form.class);
+                startActivity(intent);
+
+            }
+
 
         });
     }
