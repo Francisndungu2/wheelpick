928005bc50cffec582fa231b475ed8e66891c33e
Francisndungu2 <francianmbugua@gmail.com>
2021-01-08 02:42:05 +0300

    AddIntent

--- app/src/main/java/com/example/wheelpick/form.java
+++ app/src/main/java/com/example/wheelpick/form.java
@@ -2,11 +2,14 @@ package com.example.wheelpick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class form extends AppCompatActivity {
+
+    private Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,16 @@ public class form extends AppCompatActivity {
         next = (Button)findViewById(R.id.open);
         next.setOnClickListener(new View.OnClickListener() {
 
+            @Override
 
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(form.this, about.class);
+                startActivity(intent);
+
+            }
+
+        });
         }
     }
-}
